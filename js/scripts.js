ece17cd321d5ece4b1f3bef024943793416599de
Anastasia Han <anastasia.han@yahoo.com>
2021-11-18 16:58:39 -0500

    It will contain basic business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,27 @@
 //business logic
-function beepboop(number) {
+let wordArray1 = [];
+let wordArray2 = [];
+let wordArray3= [];
+var number = parseInt($('input#text').val());
 
+
+function beepboop(element) {
+  for (let i= 0; i <= number; i++) {
+    const element = i.toString();
+    if (element.includes(3)) {
+      wordArray1.push("Won't you be my neighbor?");
+    } else if (element.includes(2)) {
+      wordArray1.push('Boop!');
+    } else if (element.includes(1)) {
+      wordArray1.push("Beep");
+    } else {
+      wordArray1.push(element);
+    }
+  }
+  return wordArray1;
 }
+  
+
 
 
 //UI logic
